af6982460ac7a03d363107b3692ed8038ba6f7d6
vivek mhaske <mhaske.vivek18@gmail.com>
2026-06-12 18:54:45 +0530

    Initial commit

---
+++ Hellow.java
@@ -0,0 +1,14 @@
+class Test{
+
+    public static void main(String... args)
+     {
+        byte x=10;
+        switch (x) 
+        {
+            case 97:
+            case 98:
+            case 99:
+            case 'a':
+        }
+    }
+}
---
+++ practice.java
@@ -0,0 +1,131 @@
+// class june_7_{
+
+//     public static void main(String... args) {
+//     float f= 123.456f;
+//     System.out.println("Hello World");
+    
+       
+        
+//     }
+// }
+// class june_7_{
+
+//     public static void main(String... args) {
+//     float f= 123.456f;
+//     double d= 0123.456;
+//     System.out.println("Hello World");
+    
+       
+        
+//     }
+// }
+// class june_7_{
+
+//     public static void main(String... args) {
+//     char c=65536;
+//     System.out.println("Hello World");
+    
+       
+        
+//     }
+// }
+// class practice{
+//     public static void main(String... args){
+//         System.out.println(1 + "-"+ 1*1);
+//         System.out.println(2+"-"+2*2);
+//         System.out.println(3+"-"+3*3);
+//     }
+ 
+
+// }
+// class practice{
+//     int _=10;
+//     System.out.println(_)
+// }
+// till java 8 it was allowed in 9+ it is not allowed now
+
+
+// QQ -- 11//
+
+// class practice {
+//     public static void main(String... args){
+//         for (int i= 1; i <= 100; i++) {
+//             System.out.println(i);
+            
+//         }
+//     }
+
+// }
+
+// QQ -- 22//
+
+// class practice {
+//     public static void main(String... args){
+//         for(int i=1; i<=100;i++)
+//             if (i%2==0){
+//              System.out.println(i);
+//             }
+
+//     }
+// }
+
+// QQ -- 33//
+
+// class practice{
+//     public static void main(String... args){
+//         for(int i=1;i<=100;i++)
+//              if (i%2!=0)
+//                System.out.println(i);
+        
+//     }
+// }
+
+// QQ -- 44//
+// class practice
+// {
+//     public static void main(String... args)
+//     {
+//         int w=1;
+//         int i=4;
+//         int y=w++;
+//      System.out.println(i+"*"+y+"="+4*y);
+//     }
+// }
+
+// class practice
+// {
+//     public static void main(String... args)
+//     {
+//         int i=4;
+//      System.out.println(i+"*"+1+"="+4*1);
+//      System.out.println(i+"*"+2+"="+4*2);
+//      System.out.println(i+"*"+3+"="+4*3);
+//      System.out.println(i+"*"+4+"="+4*4);
+//      System.out.println(i+"*"+5+"="+4*5);
+//      System.out.println(i+"*"+6+"="+4*6);
+//      System.out.println(i+"*"+7+"="+4*7);
+//      System.out.println(i+"*"+8+"="+4*8);
+//      System.out.println(i+"*"+9+"="+4*9);
+//      System.out.println(i+"*"+10+"="+4*10);
+//     }
+// }
+
+// class practice
+// {
+//     public static void main(String... args)
+//     {
+//         for(int i=1;i<=10;i++)
+//         // int i=4;
+//      System.out.println("4*"+i+"="+4*i);
+     
+//     }
+// }
+
+// QQ -- 55//
+
+class practice{
+    public static void main(String... args){
+        for(int i=1;i<=10;i++)
+            System.out.println("4*"+i+"="+4*i);
+    }
+}
